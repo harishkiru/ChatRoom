20ba5d86d0f6e4761c465540cda3a91c440ea57d
Harish <harish.kirubaharan@ontariotechu.net>
2023-04-05 05:08:58 -0400

    dev

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,35 +1,26 @@
 let ws;
-
-function newRoom(){
-    // calling the ChatServlet to retrieve a new room ID
-    let callURL= "http://localhost:8080/WSChatServer-1.0-SNAPSHOT/chat-servlet";
-    fetch(callURL, {
-        method: 'GET',
-        headers: {
-            'Accept': 'text/plain',
-        },
-    })
-        .then(response => response.text())
-        .then(response => enterRoom(response)); // enter the room with the code
-}
-function enterRoom(code){
-
-    // refresh the list of rooms
-
-    // create the web socket
+function enterRoom() {
+    console.log("?????");
+    let code = document.getElementById("code").value;
+    console.log(code);
     ws = new WebSocket("ws://localhost:8080/WSChatServer-1.0-SNAPSHOT/ws/"+code);
 
-
-    // parse messages received from the server and update the UI accordingly
     ws.onmessage = function (event) {
         console.log(event.data);
-        // parsing the server's message as json
         let message = JSON.parse(event.data);
-
-        // handle message
-
-        }
+        document.getElementById("log").value += "[" + timestamp() + "] " + message.message + "\n";
+    }
 }
+document.getElementById("input").addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        let request = {"type":"chat", "msg":event.target.value};
+        ws.send(JSON.stringify(request));
+        event.target.value = "";
+    }
+});
 
-
-
+function timestamp() {
+    var d = new Date(), minutes = d.getMinutes();
+    if (minutes < 10) minutes = '0' + minutes;
+    return d.getHours() + ':' + minutes;
+}
