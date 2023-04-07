5eb76f5c9336db625da8824a4b4d631ce7820995
Harish <harish.kirubaharan@ontariotechu.net>
2023-04-07 03:08:49 -0400

    dev

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -11,6 +11,39 @@ function enterRoom() {
         document.getElementById("log").value += "[" + timestamp() + "] " + message.message + "\n";
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const activeChatRoomsContainer = document.getElementById('activeChatRooms');
+    const apiEndpoint = 'http://localhost:8080/WSChatServer-1.0-SNAPSHOT/api/endpoints/activeRooms';
+
+    fetchActiveChatRooms();
+
+    async function fetchActiveChatRooms() {
+        try {
+            const response = await fetch(apiEndpoint);
+            const data = await response.text();
+            //Remove first and last character (square brackets)
+            const chatRooms = data.substring(1, data.length - 1).split(',').map(room => room.trim());
+            displayActiveChatRooms(chatRooms);
+        } catch (error) {
+            console.error('Error fetching active chat rooms:', error);
+        }
+    }
+
+
+    function displayActiveChatRooms(chatRooms) {
+        const ul = document.createElement('ul');
+        chatRooms.forEach((room) => {
+            const li = document.createElement('li');
+            li.textContent = room;
+            ul.appendChild(li);
+        });
+        activeChatRoomsContainer.appendChild(ul);
+    }
+});
+
+
+
 document.getElementById("input").addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
         let request = {"type":"chat", "msg":event.target.value};
